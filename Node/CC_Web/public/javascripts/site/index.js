610e23a883af97671692889ece4a432e9ddb6ea6
jmcabee <jmcabeejr@gmail.com>
2018-01-02 22:29:25 -0500

    move web to node directory

---
+++ Node/CC_Web/public/javascripts/site/index.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    $.ajax({
+        method: 'GET',
+        url: baseUrl + 'RobotPing'
+    }).complete(function (data, status) {
+        var statusLabel = $('.statusPiRobot');
+        var cls = 'text-info';
+        var txt = 'Online';
+
+        if (status != 'success') {
+            cls = 'text-warning';
+            txt = 'Offline';
+        }
+
+        statusLabel.text(txt).removeClass('text-muted').addClass(cls);
+    });
+
+    $.ajax({
+        method: 'GET',
+        url: baseUrl + 'GaragePing'
+    }).complete(function (data, status) {
+        var statusLabel = $('.statusPiGarage');
+        var cls = 'text-info';
+        var txt = 'Online';
+
+        if (status != 'success') {
+            cls = 'text-warning';
+            txt = 'Offline';
+        }
+
+        statusLabel.text(txt).removeClass('text-muted').addClass(cls);
+    });
+});
---
+++ Node/CC_Web/public/javascripts/site/site.js
@@ -0,0 +1,10 @@
+﻿var baseUrl = document.location.origin + '/proxy/';
+var baseUrlNoProxy = document.location.origin + '/';
+
+function showLoader() {
+    $('.fill').show();
+}
+
+function hideLoader() {
+    $('.fill').hide();
+}
